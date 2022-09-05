afa6bdcf78028f0329c8a5b2cb042fd119b5f55c
pkulik0 <pkulik0@icloud.com>
2022-09-05 19:04:49 +0200

    Move definitions to suite.h

--- tests/suite.h
+++ tests/suite.h
@@ -24,12 +24,51 @@ typedef struct TestGroup {
     struct TestGroup* next;
 } test_group_t;
 
-void run_tests(const char* group_name, test_group_t* tests);
-
 #define TESTS_NEW(X, Y, Z) test_group_t* (X) = test_new((Y), (Z))
-test_group_t* test_new(const char* test_name, test_ptr test);
+test_group_t* test_new(const char* test_name, test_ptr test) {
+    test_group_t* group = malloc(sizeof *group);
+
+    group->test_name = test_name;
+    group->test = test;
+    group->next = NULL;
+
+    return group;
+}
 
 #define TESTS_ADD(X, Y, Z) (X) = test_add((X), test_new((Y), (Z)))
-test_group_t* test_add(test_group_t* tests, test_group_t* new);
+test_group_t* test_add(test_group_t* tests, test_group_t* new) {
+    if(!tests || !new) return NULL;
+
+    new->next = tests;
+    return new;
+}
+
+void run_tests(const char* group_name, test_group_t* tests) {
+    int count = 0;
+    int failed = 0;
+
+    printf("Testing group %s\n", group_name);
+
+    while(tests) {
+        printf("Test #%d \t%s\t", count++, tests->test_name);
+
+        int line = tests->test();
+        if(line == 0) {
+            printf("[SUCCESS]\n");
+        } else {
+            printf("[FAILURE] (line %d)\n", line);
+            failed++;
+        }
+
+        tests = tests->next;
+    }
+
+    printf("Group finished running.\n");
+
+    if(failed)
+        printf("%d out of %d tests failed.\n", failed, count);
+    else
+        printf("All %d tests passed.", count);
+}
 
 #endif //ASDF_SUITE_H
